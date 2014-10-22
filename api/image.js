da316224e7a440d30c944a0ecd17e65e1d7528d9
Jilles Soeters <info@jilles.me>
2014-10-22 20:31:17 +0100

    Update API to do -all- the logic

--- api/image.js
+++ api/image.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var request = require('request').defaults({ encoding: null });
-var url = require('url');
 
-var h = {
+var headers = {
   'Referer': null,
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
 };
+
+var result;
 
 function error (response) {
   response.writeHead(404);
@@ -14,26 +15,37 @@ function error (response) {
 }
 
 http.createServer(function (req, res) {
-    var img = url.parse(req.url, true).query.img;
+  var API = 'http://www.omdbapi.com';
+  var url = API + req.url + '&plot=full';
+  var start = new Date().getTime();
 
-    if (!img || img === "N/A") {
-      error(res);
-      return;
+  // Get the omdb data
+  request.get({ url: url }, function (err, omdbResponse, omdbData) {
+
+    if (!err && omdbResponse.statusCode === 200) {
+      // Assign the result to the omdb api data
+      result = JSON.parse(omdbData.toString());
+
+      // Make another request, to the imdb image
+      request.get({ url: result.Poster, headers: headers }, function (err, imdbResponse, imdbData) {
+
+        if (!err && imdbResponse.statusCode === 200) {
+          // Base64 encode the poster
+          result.Poster = 'data:image/jpeg;base64,' + imdbData.toString('base64');
+          // Set response header to 200 and content type to JSON
+          res.writeHead(200, {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:8000' // This need fixing on localhost..
+          });
+
+          var end = new Date().getTime();
+          // Set the time to ms execution time
+          result.time = end - start;
+          // End the request
+          res.end(JSON.stringify(result));
+        }
+      });
     }
-
-    request.get({ url: img, headers: h }, function (err, response, body) {
-
-      if (response === undefined || response.statusCode !== 200) {
-        error(res);
-        return;
-      } else {
-        var i = new Buffer(body);
-        res.writeHead(200, {
-        'Content-Type': 'image/jpeg',
-        'Access-Control-Allow-Origin': 'http://localhost:8000' // This need fixing on localhost..
-        });
-        res.end('data:image/jpeg;base64,' + body.toString('base64'));
-      }
-    });
+  });
 
 }).listen(3020);
