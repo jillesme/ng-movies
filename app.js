b3f78fd0f55763c0b821c86423f78b06f9f071a4
Jilles Soeters <info@jilles.me>
2014-10-27 23:29:05 +0000

    Move to absolute path

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function RouterConfig ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: './views/search-view.html',
+        templateUrl: 'views/search-view.html',
         controller: 'SearchController',
         controllerAs: 'vm'
       })
       .when('/movie/:id', {
-        templateUrl: './views/movie-view.html',
+        templateUrl: 'views/movie-view.html',
         controller: 'MovieController',
         controllerAs: 'vm',
         resolve: {
