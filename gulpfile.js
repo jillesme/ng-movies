27c96437f294b067f747ea1a13e91ebee6933b08
Jilles Soeters <info@jilles.me>
2014-10-29 20:23:36 +0000

    Move to a src / dist build

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,98 @@
 var gulp = require('gulp');
 var serv = require('gulp-webserver');
-var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var preprocess = require('gulp-preprocess');
+var sass = require('gulp-sass');
 
 var paths = {
-  javascript: './js/**/*.js'
+  scripts: [
+    'public/src/app.js',
+    'public/src/js/**/*.js'
+  ],
+  libs: [
+    'public/lib/angular/angular.js',
+    'public/lib/angular-route/angular-route.js'
+  ],
+  styles: [
+    'public/src/scss/*.scss'
+  ],
+  index: [
+    './public/src/index.html'
+  ],
+  views: [
+    './public/src/views/*.html'
+  ]
 };
 
+gulp.task('html:dev', function() {
+  gulp.src(paths.index)
+    .pipe(preprocess({ context: { build: 'dev' } }))
+    .pipe(gulp.dest('./public/dist/'));
+
+  gulp.src(paths.views)
+    .pipe(preprocess({ context: { build: 'dev' } }))
+    .pipe(gulp.dest('./public/dist/views/'));
+});
+
+gulp.task('sass:dev', function () {
+  gulp.src(paths.styles)
+    .pipe(sass())
+    .pipe(gulp.dest('./public/dist/css'));
+});
+
+gulp.task('scripts:dev', function () {
+  return gulp.src(paths.scripts)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/dist/js'));
+});
+
+gulp.task('libs:dev', function () {
+  return gulp.src(paths.libs)
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest('public/dist/js'));
+});
+
+
+gulp.task('html:prod', function() {
+  gulp.src(paths.index)
+    .pipe(preprocess({ context: { build: 'prod' } }))
+    .pipe(gulp.dest('./public/dist/'));
+
+  gulp.src(paths.views)
+    .pipe(preprocess({ context: { build: 'prod' } }))
+    .pipe(gulp.dest('./public/dist/views/'));
+});
+
+gulp.task('scripts:prod', function () {
+  return gulp.src(paths.scripts)
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'));
+});
+
+gulp.task('libs:prod', function () {
+  return gulp.src(paths.libs)
+    .pipe(concat('lib.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'));
+});
+
 gulp.task('server', function () {
-  return gulp.src('./')
+  return gulp.src('public/dist')
     .pipe(serv());
 });
 
 gulp.task('api', function () {
   return nodemon({
-    script: './api/image.js'
+    script: 'api/image.js'
   });
 });
 
 gulp.task('jshint', function () {
-  return gulp.src(paths.javascript)
+  return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
@@ -27,5 +100,18 @@ gulp.task('jshint', function () {
 gulp.task('default', [
   'server',
   'api',
+  'html:dev',
+  'sass:dev',
+  'scripts:dev',
+  'libs:dev',
   'jshint'
 ]);
+
+gulp.task('build', [
+  'server',
+  'api',
+  'scripts:prod',
+  'libs:prod',
+  'jshint'
+]);
+
